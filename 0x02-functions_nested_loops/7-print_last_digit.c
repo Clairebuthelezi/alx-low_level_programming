e14ab64741729119a7a2b5e0156baa295e25c4ee
Clairebuthelezi <aphiweclaire@gmail.com>
2023-02-21 16:13:07 -0800

    7 print

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,11 +3,11 @@
 /**
  * print_last_digit - prints the last digit
  * @n: the number
- * Return: value of the last digit 
+ * Return: value of the last digit
  */
 int print_last_digit(int n)
 {
-int ld = n% 10;
+int ld = n % 10;
 
 if (ld < 0)
 	ld *= -1;
