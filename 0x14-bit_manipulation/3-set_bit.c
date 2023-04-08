04283fd6f6a649aec99c66d221f7f794fc8eb405
Clairebuthelezi <aphiweclaire@gmail.com>
2023-04-08 13:59:39 -0700

    3set

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,14 +2,19 @@
 #include <stdio.h>
 
 /**
- * main - bit to 1 at a given index
- *
+ * set_index - bit to 1 at a given index
+ * @n: pointer of an unsigned long int.
+ * @index: index of the bit.
  * Return: (-1)
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int t;
 
+	if (index > 63)
+		return (-1);
+
 	t = 1 << index;
+	*n = (*n | t);
 	return ((*n & ~t) | (t));
 }
