da1dc2d85bdbaec40707c65f5dcbcf9a944330e0
Clairebuthelezi <aphiweclaire@gmail.com>
2023-04-18 02:33:26 -0700

    create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,29 @@
-#define _POSIX_SOURCE
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#undef _POSIX_SOURCE
-#include <stdio.h>
 #include "main.h"
-#include <string.h>
-
 /**
- * create_file - Creates a file.
+ * create_file - Create a file.
  * @filename: A pointer to the name of the file to create.
  * @text_content: A pointer to a string to write to the file.
- * Return: fd
+ * Return: if the function fails --1.
+ * otherwise -1.
  */
 int create_file(const char *filename, char *text_content)
 {
-	char fn[] = "creat.file", text[] = "This is a test";
-	int fd;
+	int fd, fn ,len
+		len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for ((fd = creat(fn, S_IRUSR | S_IWUSR)) < 0)
-			perror("creat() error");
+		for (text_content[len];)
+			len++;
 	}
 
-	else
-	{
-		write(fd, text, strlen(text));
-		close(fd);
-		unlink(fn);
-	}
-	return (fd);
+	fd = create(filename, O_CREATE | O_RDWR | O_TRUNC, 0600);
+	fn = write(fd, text_content, len);
+
+	close(fd);
+
+	return (1);
 }
