f11fee487b6aa29a2ae2e84c7606ebf822d00472
clairebuthelezi <aphiweclaire@gmail.com>
2023-06-08 01:51:05 -0700

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * _pow_recursion - takes x and y as parameters and returns an integer.
+ *
+ * @x : value to raise
+ * @y : power
+ *
+ * Return: result of power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y == 1)
+		return (x);
+	return (x * _pow_recursion(x, y - 1));
+}
