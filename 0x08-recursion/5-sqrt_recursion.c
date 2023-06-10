f137bb650c9742f57b17855f8c68b24d567767c0
clairebuthelezi <aphiweclaire@gmail.com>
2023-06-10 04:15:16 -0700

    squere

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,21 +11,28 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 
-	int start = 0, end = n;
+	if (n == 0 || n == 1)
+		return (n);
+
+	int start = 1;
+	int end = n;
+	int result = -1;
 
 	while (start <= end)
 	{
-		int mid = start + (end - start) / 2;
-		int square = mid * mid;
+		int mid = (start + end) / 2;
 
-		if (square == n)
+		if (mid * mid == n)
 			return (mid);
-		else if (square < n)
+		else if (mid * mid < n)
+		{
 			start = mid + 1;
+			result = mid;
+		}
 		else
+		{
 			end = mid - 1;
+		}
 	}
-	return (-1);
+	return (result);
 }
-
-
