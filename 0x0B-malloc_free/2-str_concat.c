c50931723640d52e0121236710767ff9044e4d7a
clairebuthelezi <aphiweclaire@gmail.com>
2023-06-13 12:21:28 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,12 +11,14 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+	char *concat;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	char *concat = strdup(s1);
+	concat = strdup(s1);
 
 	if (concat == NULL)
 		return (NULL);
