4d6595493bfcf8519de1ea2f0ee85729ab968fea
clairebuthelezi <aphiweclaire@gmail.com>
2023-07-01 07:08:58 -0700

    lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -8,3 +8,39 @@
  * @str: string to put in the new node
  *
  * Return: address of the new element, or NULL if
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node = (list_t *)malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+
+		return (NULL);
+
+	char *str_copy = strdup(str);
+
+	if (str_copy == NULL)
+	{
+		free(new_node);
+
+		return (NULL);
+	}
+new_node->str = str_copy;
+
+new_node->next = NULL;
+
+if (*head == NULL)
+{
+	*head = new_node;
+} else
+{
+	list_t *current = *head;
+
+	while (current->next != NULL)
+		current = current->next;
+
+	current->next = new_node;
+
+	return (new_node);
+}
