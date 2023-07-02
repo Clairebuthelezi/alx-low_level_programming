5de7ea72a9998e7a623d60d4070b629794c1af38
clairebuthelezi <aphiweclaire@gmail.com>
2023-07-02 08:49:26 -0700

    new

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_quote - It uses the printf function to print
+ * the desired quote with line breaks.
+ *
+ * Return : 0
+ */
+
+void print_quote(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
+// Rest of the main function code
+int main(void)
+{
+	print_quote();
+
+	return (0);
+}
